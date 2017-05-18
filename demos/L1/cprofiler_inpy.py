d2d71e6dfaea00567cff351a664cbe9c9deff19b
stevegao <gaojiahua_heu@163.com>
2017-05-18 20:16:36 +0800

    L1  change in lesson Signed-off-by: stevegao <gaojiahua_heu@163.com>

--- demos/L1/cprofiler_inpy.py
+++ demos/L1/cprofiler_inpy.py
@@ -1,6 +1,7 @@
 import cProfile
 import re
 def test():
-	for i in xrange(10**6):
-		re.compile("foo|bar")	
+	for j in xrange(10):
+		for i in xrange(10**6):
+			re.compile("foo|bar")	
 cProfile.run('test()')
